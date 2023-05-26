9fa9b1f486b91740bca5936e39517688cabe36f2
Nash <panashemhondeh@gmail.com>
2023-05-26 02:05:55 +0200

    Buttons now change to blue or red when clicked depending if true or false

--- src/Components/Quiz/index.jsx
+++ src/Components/Quiz/index.jsx
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { useQuiz } from "../QuizHook";
-import  QuizQuestion  from "../QuizQuestion";
+import QuizQuestion from "../QuizQuestion";
+import "./index.scss"
 
 const index = () => {
-  const { quizData } = useQuiz(); //to get the quiz questions from the useQuiz hook
-  const [question, setQuestion] = useState({});
+  const { quizData, difficulty, questionsLimit, categories } = useQuiz(); //to get the quiz questions from the useQuiz hook
+  const showCategories = () => {
+    const categoriesStr = Object.entries(categories)
+      .filter((category) => category[1])
+      .map((category) => category[0])
+      .toString();
+    if (categoriesStr === ""){
+      return "GENERAL KNOWLEDGE"
+    }
+    return categoriesStr.replaceAll("_"," ").replaceAll(",",",  ").toUpperCase()
+  };
   useEffect(() => {
-    console.log(quizData[0])
+    console.log(
+      showCategories()
+    );
   }, []);
   return (
     <>
-      <div>Quiz</div>
-      {quizData.map((question,index)=>{
-        return (<QuizQuestion 
-            questionNr={`Question ${index}/${quizData.length}`} 
+      <h1>Quiz</h1>
+      <div className="details container">
+        <h5>{difficulty.toUpperCase()}</h5>
+        <h5>{questionsLimit} QUESTIONS TOTAL</h5>
+        <h5>
+          {showCategories()}
+        </h5>
+      </div>
+      {quizData.map((question, index) => {
+        return (
+          <QuizQuestion
+            questionNr={`Question ${index + 1}/${quizData.length}`}
             question={question.question}
-            answers={[...question.incorrectAnswers,question.correctAnswer].sort((a,b)=>0.5-Math.random())}
+            answers={[
+              ...question.incorrectAnswers,
+              question.correctAnswer,
+            ].sort((a, b) => 0.5 - Math.random())}
             correctAnswer={question.correctAnswer}
             category={question.category}
-            ></QuizQuestion>)
+            key={index}
+          ></QuizQuestion>
+        );
       })}
-     
     </>
   );
 };
--- src/Components/QuizQuestion/index.jsx
+++ src/Components/QuizQuestion/index.jsx
@@ -2,22 +2,31 @@ import React, { useState } from "react";
 import "./index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { library } from '@fortawesome/fontawesome-svg-core';
-library.add(faCheck,faXmark); //this removes annoying console message "cannot find icon"
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faCheck, faXmark); //this removes annoying console message "cannot find icon"
 
 const index = ({ questionNr, question, answers, correctAnswer, category }) => {
-  const [className, setClassName] = useState("answer");
-  const handleClick = (answer) => {
-    setClassName("answer answered")
+  const [className, setClassName] = useState("answer notAnswered");
+  const [qnIsAnswered, setQnIsAnswered] = useState(false);
+  const handleClick = (event) => {
+    !qnIsAnswered &&
+      (event.target.style.backgroundColor =
+        event.target.id === "correct" ? "#8a7fb5" : "red"); //to change color of the clicked div
+    setClassName("answer answered");
+    setQnIsAnswered(true);
   };
   return (
     <div className="question--container">
-
       <em>{questionNr}</em>
       <h2>{question}</h2>
-      {answers.map((answer) => (
-        <div className={className} onClick={() => handleClick(answer)}>
-          <div>{answer}</div>
+      {answers.map((answer, index) => (
+        <div
+          key={index}
+          className={className}
+          onClick={handleClick}
+          id={answer === correctAnswer ? "correct" : "wrong"} //to add a correct or false id to the div
+        >
+          <div className="answer-txt">{answer}</div>
           <FontAwesomeIcon
             icon={answer === correctAnswer ? faCheck : faXmark}
           />
