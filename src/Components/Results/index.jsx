11c6ded6def11d2c722985f9543e9555f1004128
Nash <panashemhondeh@gmail.com>
2023-07-22 08:43:05 +0200

    added difficulty and category in results tab

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -3,17 +3,23 @@ import CircularProgress from "../CircularProgress";
 import { useQuiz } from "../Hooks/QuizHook";
 import { useNavigate } from "react-router-dom";
 import "./index.scss";
+import { showCategories } from "../Quiz";
 
 const index = () => {
-  const { questionsLimit, correctQnsCount } = useQuiz();
+  const { questionsLimit, correctQnsCount, difficulty, categories } = useQuiz();
   const navigate = useNavigate();
+  
   return (
     <div className="results-container">
       <CircularProgress />
       <span>
-        You answered {correctQnsCount} {correctQnsCount==1?"question":"questions"} out of {questionsLimit}{" "}
-        correct!!
+        You answered {correctQnsCount}{" "}
+        {correctQnsCount == 1 ? "question" : "questions"} out of{" "}
+        {questionsLimit} correct!!
       </span>
+      <br />
+      <span>Difficulty : {difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}</span>
+      <span>Categories : {showCategories(categories)}</span>
       <div className="toHome btn" onClick={() => navigate("/")}>
         Home
       </div>
